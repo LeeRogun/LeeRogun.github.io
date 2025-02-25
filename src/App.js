4274ead46f1553e2db693e3a8d7fe7cf71892172
LeeDongKwang <korkor5863@gmail.com>
2025-02-25 16:14:37 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Portfolio from './portfolio'
+import { BrowserRouter } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+        <BrowserRouter basename="/my-portfolio">
+          <Portfolio/> 
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default App
