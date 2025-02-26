9451e43c2af3c0e62615e18568308db134854930
LeeDongKwang <korkor5863@gmail.com>
2025-02-26 19:35:13 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import Portfolio from './portfolio'
+import React from 'react';
+import Portfolio from './portfolio';
 import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-        <BrowserRouter basename="/my-portfolio">
-          <Portfolio/> 
-        </BrowserRouter>
-    </>
-  )
-}
+    <BrowserRouter basename="/my-portfolio">
+      <Portfolio />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
